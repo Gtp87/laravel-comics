a4568143f4fcbc9baee7945530f73501b0b42a7f
Gtp87 <fabio.giannitrapani@gmail.com>
2022-02-11 16:21:43 +0100

    end header

---
+++ resources/views/guest/home.blade.php
@@ -0,0 +1,16 @@
+@extends('guest.layout.base')
+
+@section('documentTitle')
+    {{$nomePagina}}
+@endsection
+
+@section('content')
+
+  <main class="products">
+
+  </main>
+@endsection
+
+@section('scripts')
+  <script src="{{asset('js/app.js')}}"></script>
+@endsection
---
+++ resources/views/guest/partials/header.blade.php
@@ -0,0 +1,19 @@
+<header class="container">
+    <div class="header-sx">
+        <img src="{{asset('img/dc-logo.png')}}" alt="logo-dc">
+    </div>
+    <div class="header-dx">
+        <ul>
+            <li> <a href="{{route('characters')}}">characters</a></li>
+            <li> <a href="{{route('comics')}}">comics</a></li>
+            <li> <a href="{{route('movies')}}">movies</a></li>
+            <li> <a href="{{route('tv')}}">tv</a></li>
+            <li> <a href="{{route('games')}}">games</a></li>
+            <li> <a href="{{route('collectibles')}}">collectibles</a></li>
+            <li> <a href="{{route('videos')}}">videos</a></li>
+            <li> <a href="{{route('fans')}}">fans</a></li>
+            <li> <a href="{{route('news')}}">news</a></li>
+            <li> <a href={{route('shop')}}"">shop</a></li>
+        </ul>
+    </div>
+</header>

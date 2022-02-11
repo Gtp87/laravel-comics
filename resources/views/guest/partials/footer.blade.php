1a21431577379409b6f5e3514c2c12f2dc87dc4d
Gtp87 <fabio.giannitrapani@gmail.com>
2022-02-11 17:53:33 +0100

    end footer

---
+++ resources/views/guest/partials/footer.blade.php
@@ -0,0 +1,104 @@
+<footer>
+    <div class="footer-top">
+      <div class="container">
+        <ul>
+            <li>
+                <img src="{{asset('img/buy-comics-digital-comics.png')}}" alt="">
+                <h4><a href="">digital comics</a></h4>
+            </li>
+            <li>
+                <img src="{{asset('img/buy-comics-merchandise.png')}}" alt="">
+                <h4><a href="">dc merchandise</a></h4>
+            </li>
+            <li>
+                <img src="{{asset('img/buy-comics-subscriptions.png')}}" alt="">
+                <h4><a href="">subscription</a></h4>
+            </li>
+            <li>
+                <img src="{{asset('img/buy-comics-shop-locator.png')}}" alt="">
+                <h4><a href="">comic shop locator</a></h4>
+            </li>
+            <li>
+                <img src="{{asset('img/buy-dc-power-visa.svg')}}" alt="">
+                <h4><a href="">dc power visa</a></h4>
+            </li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-center">
+      <div class="container">
+        <div class="background-logo clearfix">
+          <div class="first-col">
+            <h3>dc comics</h3>
+              <ul class="footer-list">
+                <li class="footer-list-item"><a>Characters</a></li>
+                <li class="footer-list-item"><a>Comics</a></li>
+                <li class="footer-list-item"><a>Movies</a></li>
+                <li class="footer-list-item"><a>TV</a></li>
+                <li class="footer-list-item"><a>Games</a></li>
+                <li class="footer-list-item"><a>Videos</a></li>
+                <li class="footer-list-item"><a>News</a></li>
+              </ul>
+            <h3>shop</h3>
+              <ul class="footer-list">
+                  <li class="footer-list-item"><a>Shop DC</a></li>
+                  <li class="footer-list-item"><a>Shop DC Collectibles</a></li>
+              </ul>
+          </div>
+          <div class="second-col">
+            <h3>dc</h3>
+              <ul class="footer-list">
+                <li class="footer-list-item"><a>Terms Of Use</a></li>
+                <li class="footer-list-item"><a>Privacy policy (New)</a></li>
+                <li class="footer-list-item"><a>Ad Choiches</a></li>
+                <li class="footer-list-item"><a>Advertising</a></li>
+                <li class="footer-list-item"><a>Jobs</a></li>
+                <li class="footer-list-item"><a>Subscriptions</a></li>
+                <li class="footer-list-item"><a>Talent Workshops</a></li>
+                <li class="footer-list-item"><a>CPSC Certificates</a></li>
+                <li class="footer-list-item"><a>Ratings</a></li>
+                <li class="footer-list-item"><a>Shop Help</a></li>
+                <li class="footer-list-item"><a>Contact Us</a></li>
+              </ul>
+          </div>
+          <div class="third-col">
+            <h3>sites</h3>
+              <ul class="footer-list">
+                <li class="footer-list-item"><a>DC</a></li>
+                <li class="footer-list-item"><a>MAD Magazine</a></li>
+                <li class="footer-list-item"><a>DC Kids</a></li>
+                <li class="footer-list-item"><a>DC Universe</a></li>
+                <li class="footer-list-item"><a>DC Power Visa</a></li>
+              </ul>
+          </div>
+        </div>
+          </div>
+    </div>
+    <div class="footer-bottom">
+      <div class="container">
+        <div class="sign-up">
+            <a href="#">sign-up now!</a>
+        </div>
+        <div class="social">
+          <h3>follow us</h3>
+          <ul>
+            <li>
+                <img src="{{asset('img/footer-facebook.png')}}" alt="">
+            </li>
+            <li>
+                <img src="{{asset('img/footer-twitter.png')}}" alt="">
+            </li>
+            <li>
+                <img src="{{asset('img/footer-youtube.png')}}" alt="">
+            </li>
+            <li>
+                <img src="{{asset('img/footer-pinterest.png')}}" alt="">
+            </li>
+            <li>
+                <img src="{{asset('img/footer-periscope.png')}}" alt="">
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>

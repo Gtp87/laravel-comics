44f275aa007c5789f7933a7b1543d92845b795e2
Gtp87 <fabio.giannitrapani@gmail.com>
2022-02-14 17:48:41 +0100

    end

--- resources/views/guest/comic.blade.php
+++ resources/views/guest/comic.blade.php
@@ -42,5 +42,71 @@
                 </div>
             </div>
         </div>
+        <div class="talent-spec-container">
+            <div class="information">
+                <div class="information-left">
+                    <div class="info-talent">
+                        <span>Talent</span>
+                    </div>
+                    <div class="info-bottom">
+                        <div class="info-title">
+                            <span>Art by:</span>
+                        </div>
+                        <div class="description-info">
+                            @foreach($comic['artists'] as $artist)
+                                @if ($loop->last)
+                                <span><a href="#">{{ $artist }}</a></span>
+                                @else 
+                                <span><a href="#">{{ $artist}}</a>,</span>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                    <div class="info-bottom">
+                        <div class="info-title">
+                            <span>Written by:</span>
+                        </div>
+                        <div class="description-info">
+                            @foreach($comic['writers'] as $writer)
+                                @if ($loop->last)
+                                <span><a href="#">{{ $writer }}</a></span>
+                                @else 
+                                <span><a href="#">{{ $writer}}</a>,</span>
+                                @endif
+                            @endforeach
+                        </div>
+                    </div>
+                </div>
+                <div class="information-right">
+                    <div class="info-title">
+                        <span>Specs</span>
+                    </div>
+                    <div class="info-bottom">
+                        <div class="info-title">
+                            <span>Series:</span>
+                        </div>
+                        <div class="description-info">
+                            <span class="uppercase"><a href="#">action comics</a></span>
+                        </div>
+                    </div>
+                    <div class="info-bottom">
+                        <div class="info-title">
+                            <span>U.S. Price:</span>
+                        </div>
+                        <div class="description-info">
+                            <span class="uppercase">{{ $comic['price'] }}</span>
+                        </div>
+                    </div>
+                    <div class="info-bottom">
+                        <div class="info-title">
+                            <span>On Sale Date:</span>
+                        </div>
+                        <div class="description-info">
+                            <span>{{ $comic['sale_date'] }}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </main>
 @endsection
--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -6,16 +6,16 @@
     </div>
     <div class="header-dx">
         <ul class="navbar">
-            <li class="navbar-item"> <a href="{{route('characters')}}">characters</a></li>
-            <li class="navbar-item"> <a href="{{route('comics')}}">comics</a></li>
-            <li class="navbar-item"> <a href="{{route('movies')}}">movies</a></li>
-            <li class="navbar-item"> <a href="{{route('tv')}}">tv</a></li>
-            <li class="navbar-item"> <a href="{{route('games')}}">games</a></li>
-            <li class="navbar-item"> <a href="{{route('collectibles')}}">collectibles</a></li>
-            <li class="navbar-item"> <a href="{{route('videos')}}">videos</a></li>
-            <li class="navbar-item"> <a href="{{route('fans')}}">fans</a></li>
-            <li class="navbar-item"> <a href="{{route('news')}}">news</a></li>
-            <li class="navbar-item"> <a href="{{route('shop')}}">shop</a></li>
+            <li class="navbar-item {{ 'characters' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('characters')}}">characters</a></li>
+            <li class="navbar-item {{ 'comics' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('comics')}}">comics</a></li>
+            <li class="navbar-item {{ 'movies' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('movies')}}">movies</a></li>
+            <li class="navbar-item {{ 'tv' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('tv')}}">tv</a></li>
+            <li class="navbar-item {{ 'games' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('games')}}">games</a></li>
+            <li class="navbar-item {{ 'collectibles' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('collectibles')}}">collectibles</a></li>
+            <li class="navbar-item {{ 'videos' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('videos')}}">videos</a></li>
+            <li class="navbar-item {{ 'fans' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('fans')}}">fans</a></li>
+            <li class="navbar-item {{ 'news' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('news')}}">news</a></li>
+            <li class="navbar-item {{ 'shop' === Route::currentRouteName() ? 'active' : '' }}"> <a href="{{route('shop')}}">shop</a></li>
         </ul>
     </div>
     
